f0ee3bd702e5b9f85dd610b2e3929f75c0630514
wy54600316 <406735460@qq.com>
2015-12-09 17:22:42 +0800

    add js

---
+++ dev/common.js
@@ -0,0 +1,23 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement('script');
+    hm.src = '//hm.baidu.com/hm.js?37aea583c033706204516f8050a4c70e';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(hm, s);
+})();
+
+$.getReqParam = function(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i"),
+        r = window.location.search.substr(1).match(reg);
+    if (r != null) return decodeURI(r[2]);
+    return "";
+}
+
+$.contactPrm = function(_surl, _furl){
+    if(_surl){
+        if(!_furl){
+            _furl = _surl;
+        }
+        return "?_surl="+_surl+"&_furl="+_furl;
+    } else return "";
+}
